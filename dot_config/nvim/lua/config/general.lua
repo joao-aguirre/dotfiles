90365ec9fadf576d3c871e89cc5ebe78fa995a52
Joao Aguirre <aguirre.eng@gmail.com>
2026-08-26 10:32:13 -0700

    Add spell checking languages settings

--- dot_config/nvim/lua/config/general.lua
+++ dot_config/nvim/lua/config/general.lua
@@ -2,9 +2,12 @@
 -- Yank to system clipboard.
 vim.opt.clipboard = "unnamedplus"
 
+-- Set English and Portuguese as the languages for spell checking.
+vim.opt.spelllang = {'en', 'pt'} 
+
 -- Enable per-project settings via `exrc`.
 vim.opt.exrc = true
 
 -- Makes the left/right arrows and the `h` and `l` keys move across lines.
-vim.opt.whichwrap:append({ "<", ">", "h", "l" })
+vim.opt.whichwrap:append("<,>,h,l")
 
