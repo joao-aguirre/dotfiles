f76ae6238d1f91648ca01e851d3933c9107b32a8
Joao Aguirre <aguirre.eng@gmail.com>
2026-08-26 10:11:06 -0700

    Update line wrap navigation in Neovim

--- dot_config/nvim/lua/config/general.lua
+++ dot_config/nvim/lua/config/general.lua
@@ -4,3 +4,7 @@ vim.opt.clipboard = "unnamedplus"
 
 -- Enable per-project settings via `exrc`.
 vim.opt.exrc = true
+
+-- Makes the left/right arrows and the `h` and `l` keys move across lines.
+vim.opt.whichwrap:append({ "<", ">", "h", "l" })
+
