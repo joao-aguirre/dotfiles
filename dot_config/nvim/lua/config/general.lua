5798c698da284b5819a6c1f54f5c033229fb257e
Joao Aguirre <aguirre.eng@gmail.com>
2026-08-27 14:31:43 -0700

    Add left and right arrows controls in insert mode
    Allows the left and right arrows to wrap lines in insert mode.

--- dot_config/nvim/lua/config/general.lua
+++ dot_config/nvim/lua/config/general.lua
@@ -3,11 +3,13 @@
 vim.opt.clipboard = "unnamedplus"
 
 -- Set English and Portuguese as the languages for spell checking.
-vim.opt.spelllang = {'en', 'pt'} 
+vim.opt.spelllang = {"en", "pt"} 
 
 -- Enable per-project settings via `exrc`.
 vim.opt.exrc = true
 
 -- Makes the left/right arrows and the `h` and `l` keys move across lines.
-vim.opt.whichwrap:append("<,>,h,l")
+-- `<` and `>` control the arrow keys in normal mode and `[` and `]` control the
+-- same arrow keys but in insert mode.
+vim.opt.whichwrap:append("<, >, [, ], h, l")
 
