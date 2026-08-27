25aa8d969847914d2b476337e6fadab7688003d5
Joao Aguirre <aguirre.eng@gmail.com>
2026-08-27 14:30:59 -0700

    Add LaTeX shortcuts for build and open PDF

--- dot_config/nvim/lua/config/lsp/latex.lua
+++ dot_config/nvim/lua/config/lsp/latex.lua
@@ -1,1 +1,29 @@
+vim.api.nvim_create_autocmd("LspAttach", {
+    callback = function(args)
+        local client = vim.lsp.get_client_by_id(args.data.client_id)
+
+        if client and client.name == "texlab" then
+            local opts = {
+                buffer = args.buf,
+                silent = true,
+            }
+
+            vim.keymap.set(
+                "n",
+                "<leader>lb",
+                "<cmd>LspTexlabBuild<cr>",
+                vim.tbl_extend("force", opts, { desc = "LaTeX build" })
+            )
+
+            vim.keymap.set(
+                "n",
+                "<leader>lp",
+                "<cmd>LspTexlabForward<cr>",
+                vim.tbl_extend("force", opts, { desc = "LaTeX open PDF" })
+            )
+        end
+    end,
+})
+
 vim.lsp.enable("texlab")
+
