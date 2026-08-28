fd85ec3a7742bb2d2dfffc39a6a4599ee913d764
Joao Aguirre <aguirre.eng@gmail.com>
2026-08-28 11:20:13 -0700

    Fix error in Neovim settings

--- dot_config/nvim/lua/config/general.lua
+++ dot_config/nvim/lua/config/general.lua
@@ -11,5 +11,5 @@ vim.opt.exrc = true
 -- Makes the left/right arrows and the `h` and `l` keys move across lines.
 -- `<` and `>` control the arrow keys in normal mode and `[` and `]` control the
 -- same arrow keys but in insert mode.
-vim.opt.whichwrap:append("<, >, [, ], h, l")
+vim.opt.whichwrap:append("<,>,[,],h,l")
 
